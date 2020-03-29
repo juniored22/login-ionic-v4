dcf4435960528e0bd4016707d330e67c60a9fa03
Edgard Junior <edgard.junior.dev@gmail.com>
2020-03-29 20:50:47 -0300

    create front-end e test post

---
+++ README.md
@@ -0,0 +1,17 @@
+# Login Ionic v4
+
+Authentication app with framework ionic  v4.
+
+# Mockup
+
+## Sign in
+
+O App de login tera os métodos de autorização por API utilizando o JWT e autenticação do facebook e google e sera responsivo utilizando os beneficios de um app hibrido
+
+
+ <img style='width: 200px; display:block;margin:0 auto' src="mockup/feito.png"></img>
+
+## Sign up
+Em processo de desenvolvimento
+
+ <img style='max-width: 600px;' src="mockup/sign-up.png"></img>
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  email: string;
+  password: string;
+
+  constructor(public alertController: AlertController, public http: HttpClient) { }
+
+
+  /**
+   * Action POST login in sistem
+   */
+  login() {
+    this.presentAlertInputEmpty()
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'content-type': 'application/json'
+      })
+    };
+
+    this.http.post<any>('http://localhost/teste-post.php', { name: "teste" }, httpOptions)
+      .subscribe((data: any) => {
+        console.log(data);
+      }
+
+      );
+  }
+
+
+  /**
+   * Input empyt 
+   * email and password
+   */
+  async presentAlertInputEmpty() {
+
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      message: 'fill in the email and password fields.',
+      buttons: ['OK']
+    });
+
+    if (this.password == undefined || this.password == '') {
+      await alert.present();
+    }
+
+    if (this.email == undefined || this.email == '') {
+      await alert.present();
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
